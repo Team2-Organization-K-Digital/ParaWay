8f96f3c85e2fbb77f6bd50ff9782fe4da07516df
Eunjun1 <ibbuni8486@gmail.com>
2025-07-18 14:34:18 +0900

    hotfix (news content 배경색 수정)
    original -> white

--- paraway/subway_main/lib/view/news_content.dart
+++ paraway/subway_main/lib/view/news_content.dart
@@ -2,16 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_link_previewer/flutter_link_previewer.dart';
 
-
 class NewsContentPage extends StatelessWidget {
   final String title;
   final String content;
 
-  const NewsContentPage({
-    Key? key,
-    required this.title,
-    required this.content,
-  }) : super(key: key);
+  const NewsContentPage({Key? key, required this.title, required this.content})
+    : super(key: key);
 
   // URL이 유효한지 검사 후 launch
   void _launchURL(String url) async {
@@ -27,10 +23,11 @@ class NewsContentPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // 단순 방식: content가 전부 URL이라 가정하고 url -> 링크로변환. 콘텐츠사이에 url은 다른방식으로
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text(title), backgroundColor: Colors.white),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        
+
         child: Column(
           children: [
             GestureDetector(
@@ -43,10 +40,11 @@ class NewsContentPage extends StatelessWidget {
                 ),
               ),
             ),
-            LinkPreview(  onLinkPreviewDataFetched: (_) {}, // onlink..는필수 무슨기능인지는모름
-              text: content) // 여기 
+            LinkPreview(
+              onLinkPreviewDataFetched: (_) {}, // onlink..는필수 무슨기능인지는모름
+              text: content,
+            ), // 여기
           ],
-        
         ),
       ),
     );
