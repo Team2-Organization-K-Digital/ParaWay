86506729a2781d18d66b8c05610559d5b9c15eae
seoyoony <yoonyoon0403@naver.com>
2025-07-18 14:24:48 +0900

    중복제거

--- paraway/subway_main/lib/view/PersonProgressPage.dart
+++ paraway/subway_main/lib/view/PersonProgressPage.dart
@@ -211,33 +211,8 @@ class PersonProgressPage extends StatelessWidget {
                   ),
                 ],
               ),
-              ElevatedButton(
-                onPressed: () {
-                  picker.DatePicker.showDateTimePicker(
-                    context,
-                    showTitleActions: true,
-                    minTime: DateTime(now.year, now.month, now.day, 6, 0),
-                    maxTime: DateTime(2025, 12, 31, 23, 0),
-                    currentTime: DateTime.now(),
-                    locale: picker.LocaleType.ko,
-                    onConfirm: (dateTime) {
-                      provider.setDateTime(dateTime);
-                    },
-                  );
-                },
-                child: Text("날짜 & 시간 선택"),
-              ),
             ],
           ),
-          SizedBox(height: 30, child: Text('6시 ~ 23시 (1시간 단위)')),
-          provider.selectedDateTime != null
-              ? Text(
-                '선택된 날짜: ${provider.selectedDateTime!.year}-${provider.selectedDateTime!.month.toString().padLeft(2, '0')}-${provider.selectedDateTime!.day.toString().padLeft(2, '0')} '
-                '${provider.selectedDateTime!.hour.toString().padLeft(2, '0')}:${provider.selectedDateTime!.minute.toString().padLeft(2, '0')}',
-                style: TextStyle(fontSize: 16),
-              )
-              : Text('날짜와 시간을 선택해주세요.'),
-          SizedBox(height: 30),
           SizedBox(height: 18),
           Row(
             children: [
@@ -276,16 +251,16 @@ class PersonProgressPage extends StatelessWidget {
                       valueColor:
                           provider.out == true
                               ? AlwaysStoppedAnimation(
-                                pred['fconfusion'] <= 80
+                                pred['fconfusion'] < 80
                                     ? Colors.green
-                                    : pred['fconfusion'] <= 120
+                                    : pred['fconfusion'] < 120
                                     ? Colors.amber 
                                     : Colors.red,
                               )
                               : AlwaysStoppedAnimation(
-                                pred['oconfusion'] <= 80
+                                pred['oconfusion'] < 80
                                     ? Colors.green
-                                    : pred['oconfusion'] <= 120
+                                    : pred['oconfusion'] < 120
                                     ? Colors.amber
                                     : Colors.red,
                               ), 
